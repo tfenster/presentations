91717750b289de99d92cc06b1d48158af45ec514
Tobias Fenster <tfenster@infoma.de>
2018-11-20 16:14:06 +0100

    add script to pull all images

---
+++ docs/201811-techdaysdockerws/src/pullall.bat
@@ -0,0 +1,12 @@
+docker pull microsoft/dotnet-samples:dotnetapp-nanoserver-sac2016
+docker pull microsoft/nanoserver:sac2016
+docker pull microsoft/iis:nanoserver-sac2016
+docker pull microsoft/mssql-server-windows-express
+docker pull microsoft/windowsservercore
+docker pull microsoft/dotnet-framework:4.7.2-sdk-windowsservercore-ltsc2016
+docker pull microsoft/aspnet:4.7.2-windowsservercore-ltsc2016
+docker pull dockersamples/mta-dev-web-builder:4.7.1
+docker pull microsoft/aspnet:4.7.1-windowsservercore-ltsc2016
+docker pull microsoft/bcsandbox:us
+docker pull mcr.microsoft.com/businesscentral/onprem
+docker pull microsoft/dynamics-nav:2018-gb
